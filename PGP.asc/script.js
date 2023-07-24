801cbe6d5fb316e465075baf055fbdae6793509f
Solanaceae <89823371+Atropa-Solanaceae@users.noreply.github.com>
2023-07-24 17:37:59 -0400

    Update script.js

--- PGP.asc/script.js
+++ PGP.asc/script.js
@@ -1,18 +1,17 @@
-function copyPGPKey() {
-    const pgpKeyText = document.querySelector('.pgp-key').innerText;
-    const tempTextArea = document.createElement('textarea');
-    tempTextArea.value = pgpKeyText;
-    document.body.appendChild(tempTextArea);
-    tempTextArea.select();
-    document.execCommand('copy');
-    document.body.removeChild(tempTextArea);
+    function copyPGPKey() {
+        const pgpKeyText = document.querySelector('.pgp-key').innerText;
+        const tempTextArea = document.createElement('textarea');
+        tempTextArea.value = pgpKeyText;
+        document.body.appendChild(tempTextArea);
+        tempTextArea.select();
+        document.execCommand('copy');
+        document.body.removeChild(tempTextArea);
 
-    const copyButton = document.querySelector('.copy-button');
-    copyButton.innerText = 'Copied';
-    copyButton.style.backgroundColor = '#a819c1b6'; // Darker color when clicked
+        const copyButton = document.querySelector('.copy-button');
+        copyButton.innerText = 'copied';
+        copyButton.style.backgroundColor = '#946df7';
 
-    setTimeout(function () {
-        copyButton.innerText = 'Copy';
-        copyButton.style.backgroundColor = '#a819cf'; // Reset the button after 2 seconds
-    }, 5000);
-}
+        setTimeout(function () {
+            copyButton.innerText = 'copy';
+        }, 2000);
+    }
